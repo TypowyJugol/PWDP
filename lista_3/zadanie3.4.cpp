07c9fe822e23f81bc58b7501e90fc3ff4aae8d8b
TypowyJugol <wojtek.roman254@gmail.com>
2019-11-06 07:24:09 +0100

    lista3

--- lista_3/zadanie3.4.cpp
+++ lista_3/zadanie3.4.cpp
@@ -1,9 +1,41 @@
 #include <iostream>
 using namespace std;
+int dzialanie, x;
+float l1, l2;
 int main()
-int dzialanie;
 {
-	cin>>dzialanie;
-	
+	while(x==0){
+		cout<<"Podaj rodzaj dzialania: 1-dodawanie, 2-mnozenie: ";
+		cin>>dzialanie;
+		if(dzialanie==0)
+		{
+			cout<<"To nie jest liczba"<<endl;
+			break;
+		}
+		else if(dzialanie==2)
+		{
+			cout<<"Wybrano mnozenie"<<endl;
+		}
+		else
+		{
+			cout<<"Wybrano dodawanie"<<endl;
+		}
+		
+		cout<<"Podaj pierwszą liczbę: ";
+		cin>>l1;
+		cout<<"Podaj drugą liczbę: ";
+		cin>>l2;
+		if(dzialanie==1)
+		{
+			cout<<"Wynik działania to: "<<l1+l2<<endl;
+		}
+		else
+		{
+			cout<<"Wynik działania to: "<<l1*l2<<endl;
+		}
+		cout<<"Czy chcesz kontynować obliczenia(Tak-0, Nie-1)?";
+		cin>>x;
+	}
+
 	return 0;
 }
