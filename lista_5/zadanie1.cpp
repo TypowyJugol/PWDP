d7adbd5bcf899c690ed2c3b9ff3f1cc7a8d38d8b
TypowyJugol <wojtek.roman254@gmail.com>
2019-11-27 00:56:48 +0100

    gra

---
+++ lista_5/zadanie1.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int pole, tura = 1, gracz, tabkolko[3], tabkrzyzyk[3], ikrz=0, ikol=0;
+    cout << "Witaj w grze w kółko i krzyżyk" << endl;
+    while (tura < 7)
+    {
+        if (gracz == 1)
+        {
+            cout << "Teraz kolej gracza 'kółko'" << endl;
+            cout << "Podaj pole na którym chcesz umieścić znak" << endl;
+            cout << "Pamiętaj, że możesz to zrobić tylko na wolnym polu" <<endl;
+            cin >> tabkolko[ikol];
+            gracz=0;
+            ikol++;
+        }
+        else
+        {
+            cout << "Teraz kolej gracza 'krzyżyk'" << endl;
+            cout << "Podaj pole na którym chcesz umieścić znak" << endl;
+            cout << "Pamiętaj, że możesz to zrobić tylko na wolnym polu" <<endl;
+            cin >> tabkrzyzyk[ikrz];
+            gracz=1;
+            ikrz++;
+        }
+        tura++;
+    }
+    if ()
+    return 0;
+}
